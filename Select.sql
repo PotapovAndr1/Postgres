1780c277ddc95bdd50384bc267e36c9b11c893f1
PotapovAndr <potapovandr@gmail.com>
2025-04-15 16:16:59 +0900

    new version

--- Select.sql
+++ Select.sql
@@ -1,10 +1,141 @@
+-- Таблица исполнителей
+CREATE TABLE artist (
+    artistid SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+-- Таблица жанров
+CREATE TABLE genre (
+    genreid SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+-- Связь "исполнитель-жанр" (многие-ко-многим)
+CREATE TABLE artist_genres (
+    artistid INT,
+    genreid INT,
+    PRIMARY KEY (artistid, genreid),
+    FOREIGN KEY (artistid) REFERENCES artist(artistid) ON DELETE CASCADE,
+    FOREIGN KEY (genreid) REFERENCES genre(genreid) ON DELETE CASCADE
+);
+
+-- Таблица альбомов
+CREATE TABLE album (
+    albumid SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    release_year SMALLINT
+);
+
+-- Связь "альбом-исполнитель" (многие-ко-многим)
+CREATE TABLE album_artist (
+    albumid INT,
+    artistid INT,
+    PRIMARY KEY (albumid, artistid),
+    FOREIGN KEY (albumid) REFERENCES album(albumid) ON DELETE CASCADE,
+    FOREIGN KEY (artistid) REFERENCES artist(artistid) ON DELETE CASCADE
+);
+
+-- Таблица треков
+CREATE TABLE track (
+    trackid SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    duration INT, 
+    albumid INT,
+    FOREIGN KEY (albumid) REFERENCES album(albumid) ON DELETE CASCADE
+);
+
+-- Таблица сборников
+CREATE TABLE collection (
+    collectionid SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    release_year SMALLINT NOT NULL
+);
+
+-- Связь "трек-сборник" (многие-ко-многим)
+CREATE TABLE track_collection (
+    trackid INT,
+    collectionid INT,
+    PRIMARY KEY (trackid, collectionid),
+    FOREIGN KEY (trackid) REFERENCES track(trackid) ON DELETE CASCADE,
+    FOREIGN KEY (collectionid) REFERENCES collection(collectionid) ON DELETE CASCADE
+);
+
+
+-- Исполнители
+INSERT INTO artist (artistid, name) VALUES
+(1, 'Leningrad'),
+(2, 'Ivanushki int'),
+(3, 'Zveri'),
+(4, 'Valeria');
+
+-- Жанры
+INSERT INTO genre (genreid, name) VALUES
+(1, 'Pop'),
+(2, 'Rock'),
+(3, 'Alternative');
+
+-- Альбомы
+INSERT INTO album (albumid, title, release_year) VALUES
+(1, 'WWW', 2008),
+(2, 'Tuchi', 2000),
+(3, 'Chasiki', 2005),
+(4, 'New', 2020);
+
+-- Связь исполнителей с жанрами
+INSERT INTO artist_genres (artistid, genreid) VALUES
+(1, 2), 
+(1, 3),  
+(2, 1),  
+(3, 2), 
+(4, 1); 
+
+-- Связь исполнителей с альбомами
+INSERT INTO album_artist (artistid, albumid) VALUES
+(1, 1),
+(2, 2),
+(4, 3),
+(3, 4);
+
+-- Треки
+INSERT INTO track (trackid, title, duration, albumid) VALUES
+(1, 'WWW', 186, 1),
+(2, 'Terminator', 177, 1),
+(3, 'Tuchi', 295, 2),
+(4, 'Topoliniy puh', 223, 2),
+(5, 'Do skoroy vstrechi', 201, 3),
+(6, 'Tramvai', 281, 3),
+(7, 'My mir', 225, 4),
+(8, 'Nova Era', 240, 4),
+(9, 'Start Again', 210, 4);
+
+-- Сборники
+INSERT INTO collection (collectionid, name, release_year) VALUES
+(1, 'Greatest Hits 2000s', 2010),
+(2, 'Hits 2000', 2016),
+(3, 'Best Rock', 2019),
+(4, 'Love Songs', 2015);
+
+-- Связь сборников с треками
+INSERT INTO track_collection (collectionid, trackid) VALUES
+(1, 1),
+(1, 3),
+(2, 2),
+(2, 4),
+(3, 1),
+(3, 5),
+(4, 4),
+(4, 6),
+(3, 7),
+(4, 8),
+(3, 9);
+
 -- 1. Название и продолжительность самого длительного трека
 SELECT title, duration
 FROM track
 ORDER BY duration DESC
 LIMIT 1;
 
--- 2. Название треков, продолжительность которых не менее 3,5 минут
+-- 2. Название треков, продолжительность которых не менее 3,5 минут (210 секунд)
 SELECT title
 FROM track
 WHERE duration >= 210;
